fcf727f1a9d7c3ff69402b3f83bfc3fc5832b1d6
Richard Khonan <richardkhonan0797@gmail.com>
2020-03-19 15:03:46 +0700

    Added rest api to access stores, items and user authentications

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from db import db
+
+from security import authentication, identity
+from resources.user import UserRegister
+from resources.item import Item, Items
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'richard'
+api = Api(app)
+
+jwt = JWT(app, authentication, identity)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+api.add_resource(Items, '/items')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(UserRegister, '/register')
+api.add_resource(StoreList, '/stores')
+api.add_resource(Store, '/store/<string:name>')
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(debug=True)
---
+++ resources/store.py
@@ -0,0 +1,38 @@
+from flask_restful import Resource, reqparse
+from models.store import StoreModel
+
+class Store(Resource):
+
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+
+        if store:
+            return store.json()
+        
+        return {'message': 'Store not found.'}, 404
+
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'Message': 'Store already exists.'}, 400
+
+        new_store = StoreModel(name)
+
+        try:
+            new_store.save_to_db()
+        except:
+            return {'message': 'An error occured creating store.'}, 500
+
+        return new_store.json(), 201
+
+    def delete(self, name):
+        store = StoreModel.find_by_name(name)
+
+        if store:
+            store.delete_from_db()
+
+        return {'message': 'Store deleted.'}
+
+class StoreList(Resource):
+    def get(self):
+        stores = [store.json() for store in StoreModel.query.all()]
+        return {'stores': stores}
